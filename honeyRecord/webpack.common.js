c5e77cbe04ad935a2fc9fd46de787bed37525346
dongxiaoping-nb <dong.xiaoping@opg.cn>
2018-07-24 15:28:57 +0800

    优化命名

--- honeyRecord/webpack.common.js
+++ honeyRecord/webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dists')
+        path: path.resolve(__dirname, 'dist')
     },
     //分内置插件和外置插件
     plugins: [
