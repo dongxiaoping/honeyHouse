ea75e212f1fb2909610ee2bdec9655e19d5a0007
dongxiaoping-nb <dong.xiaoping@opg.cn>
2018-08-20 13:19:43 +0800

    优化文章

--- honeyRecord/src/data/contentIndex.js
+++ honeyRecord/src/data/contentIndex.js
@@ -9,29 +9,32 @@
 // +----------------------------------------------------------------------
 var contentIndex =[
     {
-        title:"蜂蜜应该什么时候喝最好？早上还是晚上？",
-        images:["http://papwt4d89.bkt.clouddn.com/20180817192424.png","http://papwt4d89.bkt.clouddn.com/20180817192846.png"],
-        content:"demo.html",
-        date:"蜜之屋 2018-07-10"
+        "title":"蜂蜜应该什么时候喝最好？早上还是晚上？",
+        "images":["http://papwt4d89.bkt.clouddn.com/20180817192424.png","http://papwt4d89.bkt.clouddn.com/20180817192846.png"],
+        "content":"demo.html",
+        "visit_count":50,
+        "date":"蜜之屋 2018-07-10"
     },
     {
-        title:"好蜜要放在冰箱保存？你搞错了...",
-        images:[
+        "title":"好蜜要放在冰箱保存？你搞错了...",
+        "images":[
             "http://papwt4d89.bkt.clouddn.com/20180818155807.png",
             "http://papwt4d89.bkt.clouddn.com/20180818155854.png",
             "http://papwt4d89.bkt.clouddn.com/20180818155619.png"
         ],
-        content:"article-1.html",
-        date:"蜜之屋 2018-08-18"
+        "visit_count":50,
+        "content":"article-1.html",
+        "date":"蜜之屋 2018-08-18"
     },
     {
-        title:"为什么我不建议购买超市蜜 ，这里面有个大秘密",
-        images:[
+        "title":"为什么我不建议购买超市蜜 ，这里面有个大秘密",
+        "images":[
             "http://papwt4d89.bkt.clouddn.com/20180818162550.png",
             "http://papwt4d89.bkt.clouddn.com/20180818162455.png",
             "http://papwt4d89.bkt.clouddn.com/20180818162353.png"
         ],
-        content:"article-2.html",
-        date:"蜜之屋 2018-08-18"
+        "visit_count":50,
+        "content":"article-2.html",
+        "date":"蜜之屋 2018-08-18"
     }
 ];
--- honeyRecord/src/index.js
+++ honeyRecord/src/index.js
@@ -1,20 +1,26 @@
-var bodyOb = document.body;
-var articleString = "";
-for(var i=0;i<contentIndex.length;i++){
-    articleString += getHtmlStringByArticle(contentIndex[i],i);
-}
-bodyOb.innerHTML = articleString;
 
-for(var i=0;i<contentIndex.length;i++){
-    $("#item0b_"+i).on("tap",function(event){
-        var content = $(this).attr("content");
-        var randNum = GetRandomNum(1,9999999);
-        var pageUrl = "./page/"+content+"?recommendQCode="+recommendQCode+"&rand="+randNum;
-        window.location.href = pageUrl;
-    })
-}
+$(function(){
+    $.get("http://localhost/honeyHouse/server/public/index.php?s=article/get_online_articles",function(res,status){
+        var list = JSON.parse(res);
+        var contentIndex = list.data;
+        var bodyOb = document.body;
+        var articleString = "";
+        for(var i=0;i<contentIndex.length;i++){
+            articleString += getHtmlStringByArticle(contentIndex[i],i);
+        }
+        bodyOb.innerHTML = articleString;
 
-var recommendQCode = getUrlParam("recommendQCode");
+        var recommendQCode = getUrlParam("recommendQCode");
+        for(var i=0;i<contentIndex.length;i++){
+            $("#item0b_"+i).on("tap",function(event){
+                var content = $(this).attr("content");
+                var randNum = GetRandomNum(1,9999999);
+                var pageUrl = "./page/"+content+"?recommendQCode="+recommendQCode+"&rand="+randNum;
+                window.location.href = pageUrl;
+            })
+        }
+    });
+});
 
 function getHtmlStringByArticle(item,i){
     var imgString = "";
